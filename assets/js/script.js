9f7dcb390da671e796386947abf68f888e6579d9
simonjellvert <>
2023-07-20 11:06:03 +0000

    Add basic js to script, functioning game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,46 @@
-function runGame() {
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            runGame(this.getAttribute("data-selection"));
+        });
+    }
+});
+
+function runGame(playerChoice) {
+    const computerChoice = computerPlay();
+    alert(`You choose ${playerChoice}. Computer chose ${computerChoice}"`);
+
+    if (playerChoice === computerChoice) {
+        alert("It's a tie!");
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        alert("You win!");
+    } else {
+        alert("Computer Wins!");
+    }
+}
+
+function computerPlay() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+
+
+function compareChoice() {
 
 }
 
-function checkResult() {
+function incrementWin() {
 
 }
 
-function incrementUserScore() {
+function incrementLoss() {
 
 }
-
-function incrementComputerScore() {
-
-}
-
